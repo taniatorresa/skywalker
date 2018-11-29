8b4efa01edd48d44ab9daefc00d5c9a9e60c3dbe
Tania T <tania.torresa@hotmail.com>
2018-11-28 22:57:35 -0600

    dddd

---
+++ Assets/Scripts/control.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class control : MonoBehaviour {
+	public void CambiarEscena(string nombre)
+	{
+		print ("Cambiando la escena...");
+		SceneManager.LoadScene (nombre);
+	}
+
+	void Update()
+	{
+		print ("Saliendo del juego");
+
+	}
+
+	public void Salir(string salir)
+	{
+		print ("Saliendo del juego");
+		Application.Quit();
+	}
+}
